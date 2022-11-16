c729f0bb71725cfc5ca786ba892c30d28a24ea2c
manuel <manuelinsfran@winner.com.py>
2022-11-15 23:52:44 -0300

    se sube proyecto a repositorio

---
+++ js/app.js
@@ -0,0 +1,52 @@
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
+let carrito = {};
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchData();
+});
+
+items.addEventListener("click", (e) => {
+  addCarrito(e);
+});
+
+const fetchData = async () => {
+  try {
+    const res = await fetch("/js/api.json");
+    const data = await res.json();
+    //console.log(data);
+    pintarCards(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const pintarCards = (data) => {
+  data.forEach((producto) => {
+    templateCard.querySelector("h5").textContent = producto.title;
+    templateCard.querySelector("p").textContent = producto.precio;
+    templateCard.querySelector("img").setAttribute("src", producto.thumnailUrl);
+    templateCard.querySelector("button").dataset.id = producto.id;
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  items.appendChild(fragment);
+};
+
+const addCarrito = (e) => {
+  //console.log(e.target);
+  //console.log(e.target.classList.contains("btn-dark"));
+  if (e.target.classList.contains("btn-dark")) {
+    setCarrito(e.target.parentElement);
+  }
+  e.stopPropagation();
+};
+
+const setCarrito = (objeto) => {
+  //console.log(objeto);
+  const producto = {
+    id: objeto.querySelector(".btn-dark").dataset.id,
+  };
+  console.log(producto);
+};
